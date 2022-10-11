8fd2cae0f6a537682215d3416124f495f6944d6a
Sand82 <sandoki@abv.bg>
2022-10-11 13:48:20 +0300

    change problem solution

--- JavaAdvancedRetakeExam-15December2021/exam/src/ThroneConquering_02/Main.java
+++ JavaAdvancedRetakeExam-15December2021/exam/src/ThroneConquering_02/Main.java
@@ -3,37 +3,36 @@ package ThroneConquering_02;
 import java.util.Scanner;
 
 public class Main {
-
     private static int parisRow = 0;
     private static int parisCol = 0;
     private static int parisEnergy = 0;
     private static boolean isHelenFound = false;
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         parisEnergy = Integer.parseInt(scanner.nextLine());
 
         int n = Integer.parseInt(scanner.nextLine());
 
-        char[][] matrix = new char[n][n];
+        String[][] matrix = new String [n][];
 
-        for (int row = 0; row < n; row++) {
-
-            char[] charInput = scanner.nextLine().toCharArray();
-
-            for (int col = 0; col < n; col++) {
-
-                matrix[row][col] = charInput[col];
-
-                if (charInput[col] == 'P') {
-                    parisRow = row;
-                    parisCol = col;
+        for (int i = 0; i < n; i++) {
+            matrix[i] = scanner.nextLine().split("");
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j].equals("P")) {
+                    parisRow = i;
+                    parisCol = j;
                 }
             }
         }
 
-        while (true) {
+        while (parisEnergy > 0) {
+            if (isHelenFound) {
+                System.out.println(String.format("Paris has successfully abducted Helen! Energy left: %d", parisEnergy));
+                matrix[parisRow][parisCol] = "-";
+                break;
+            }
 
             String[] input = scanner.nextLine().split("\\s+");
 
@@ -41,7 +40,7 @@ public class Main {
             int spartansRow = Integer.parseInt(input[1]);
             int spartansCol = Integer.parseInt(input[2]);
 
-            matrix[spartansRow][spartansCol] = 'S';
+            matrix[spartansRow][spartansCol] = "S";
 
             parisEnergy--;
 
@@ -71,49 +70,37 @@ public class Main {
                     }
                     break;
             }
-
-            if (isHelenFound) {
-
-                System.out.println(String.format("Paris has successfully abducted Helen! Energy left: %d", parisEnergy));
-                matrix[parisRow][parisCol] = '-';
-                break;
-            }
-
-            if (parisEnergy <= 0) {
-
-                matrix[parisRow][parisCol] = 'X';
-                System.out.println(String.format("Paris died at %d;%d.", parisRow, parisCol));
-                break;
-            }
         }
-
-
+        if (!isHelenFound) {
+            matrix[parisRow][parisCol] = "X";
+            System.out.println(String.format("Paris died at %d;%d.", parisRow, parisCol));
+        }
 
         printMatrix(matrix);
     }
 
-    private static void checkCoordinates(int rowIndex, int colIndex, char[][] matrix) {
+    private static void checkCoordinates(int rowIndex, int colIndex, String[][] matrix) {
 
         int currRow = parisRow + rowIndex;
         int currCol = parisCol + colIndex;
 
-        if (matrix[currRow][currCol] == 'S') {
+        if (matrix[currRow][currCol].equals("S")) {
 
             parisEnergy -= 2;
 
-        } else if (matrix[currRow][currCol] == 'H') {
+        } else if (matrix[currRow][currCol].equals("H")) {
 
             isHelenFound = true;
         }
 
-        matrix[parisRow][parisCol] = '-';
+        matrix[parisRow][parisCol] = "-";
         parisRow = currRow;
         parisCol = currCol;
-        matrix[parisRow][parisCol] = 'P';
+        matrix[parisRow][parisCol] = "P";
 
     }
 
-    private static void printMatrix(char[][] matrix) {
+    private static void printMatrix(String[][] matrix) {
 
         for (int row = 0; row < matrix.length; row++) {
 
