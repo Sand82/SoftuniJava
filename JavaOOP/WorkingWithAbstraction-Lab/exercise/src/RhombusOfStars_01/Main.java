e959b983c4d45d3709b60a1f87c4848449a7b679
Sand82 <sandoki@abv.bg>
2022-10-16 12:37:15 +0300

    add new exercise

---
+++ JavaOOP/WorkingWithAbstraction-Lab/exercise/src/RhombusOfStars_01/Main.java
@@ -0,0 +1,17 @@
+package RhombusOfStars_01;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Rhombus rhombus = new Rhombus(n);
+
+        System.out.println(rhombus.getFigure());
+    }
+}
---
+++ JavaOOP/WorkingWithAbstraction-Lab/exercise/src/RhombusOfStars_01/Rhombus.java
@@ -0,0 +1,64 @@
+package RhombusOfStars_01;
+
+public class Rhombus {
+
+    private int n;
+
+    public Rhombus(int n) {
+        this.n = n;
+    }
+
+    public String getFigure() {
+
+        return printTop() + printMiddle() + printBottom();
+    }
+
+
+    private String printTop() {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int r = 1; r < n; r++) {
+
+            sb.append(repeatAndPrintString(n - r, " "))
+                    .append(repeatAndPrintString(r, "* "))
+                    .append(System.lineSeparator());
+        }
+
+        return sb.toString();
+    }
+
+    private String printMiddle() {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(repeatAndPrintString(n, "* "))
+                .append(System.lineSeparator());
+
+        return sb.toString();
+    }
+
+    private String printBottom() {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int r = 1; r < n; r++) {
+
+            sb.append(repeatAndPrintString(r, " "))
+                    .append(repeatAndPrintString(n - r, "* "))
+                    .append(System.lineSeparator());
+        }
+
+        return sb.toString();
+    }
+
+    private String repeatAndPrintString(int count, String str) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < count; i++) {
+            sb.append(str);
+        }
+
+        return sb.toString();
+    }
+}
