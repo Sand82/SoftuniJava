86d0cff547a1c5e254cd78a7eadd90145026bffa
Sand82 <sandoki@abv.bg>
2022-10-20 09:25:54 +0300

    add new exercise

---
+++ JavaOOP/Inheritance-Lab/exercise/src/RandomArrayList_04/RandomArrayList.java
@@ -0,0 +1,16 @@
+package RandomArrayList_04;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomArrayList extends ArrayList<Object> {
+
+    public Object getRandomElement() {
+
+        Random random = new Random();
+
+        int index = random.nextInt(super.size());
+        return remove(index);
+    }
+
+}
