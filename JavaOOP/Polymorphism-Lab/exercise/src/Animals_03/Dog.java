09f8b1e9ee37c09c21f50bf1630891c48d115a12
Sand82 <sandoki@abv.bg>
2022-10-24 13:35:55 +0300

    add new exercise

---
+++ JavaOOP/Polymorphism-Lab/exercise/src/Animals_03/Dog.java
@@ -0,0 +1,13 @@
+package Animals_03;
+
+public class Dog extends Animal {
+
+    public Dog(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return super.explainSelf() + System.lineSeparator() + "DJAAF";
+    }
+}
