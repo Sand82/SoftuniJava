2a6632acd21402af3e3f4c307e5a876d459214d3
Sand82 <sandoki@abv.bg>
2022-10-27 09:21:09 +0300

    add new exercise

---
+++ JavaOOP/Reflection-Lab/exercise/src/GettersAndSetters_02/Main.java
@@ -0,0 +1,4 @@
+package GettersAndSetters_02;
+
+public class Main {
+}
---
+++ JavaOOP/Reflection-Lab/exercise/src/Reflection_01/Main.java
@@ -0,0 +1,23 @@
+package Reflection_01;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        Class reflection = Reflection.class;
+        System.out.println(reflection);
+
+        Class superClass = reflection.getSuperclass();
+        System.out.println(superClass);
+
+        Class [] interfaces =  reflection.getInterfaces();
+
+        for (Class anInterface : interfaces) {
+            System.out.println(anInterface);
+        }
+
+        Object reflectionObject = reflection.getDeclaredConstructor().newInstance();
+        System.out.println(reflectionObject);
+    }
+}
