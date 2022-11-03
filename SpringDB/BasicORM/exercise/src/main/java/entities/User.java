c57a1965a663aba7fd979c04d347b18db4468b7f
Sand82 <sandoki@abv.bg>
2022-11-03 13:18:13 +0200

    create class user

---
+++ SpringDB/BasicORM/exercise/src/main/java/entities/User.java
@@ -0,0 +1,52 @@
+package entities;
+
+import java.time.LocalDate;
+
+public class User {
+
+    private long id;
+
+    private String username;
+
+    private int age;
+
+    private LocalDate registrationDate;
+
+    public User(String username, int age, LocalDate registrationDate) {
+        this.username = username;
+        this.age = age;
+        this.registrationDate = registrationDate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
