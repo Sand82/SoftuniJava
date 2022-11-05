9ff99a94b475c66c9dcf244b8919077e44c46216
Sand82 <sandoki@abv.bg>
2022-11-05 23:20:13 +0200

    add new exercise

---
+++ SpringDB/IntroductionToHibernate-Lab/Hibernate_JPA/src/main/java/Main.java
@@ -0,0 +1,20 @@
+import entities.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("school");
+
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        Student student = new Student("sand");
+        entityManager.persist(student);
+
+        entityManager.getTransaction().commit();
+        entityManager.close();
+
+    }
+}
---
+++ SpringDB/IntroductionToHibernate-Lab/Hibernate_JPA/src/main/java/entities/Student.java
@@ -0,0 +1,50 @@
+package entities;
+
+import jakarta.persistence.*;
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import java.lang.annotation.Target;
+
+@Entity()
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name ="name", length = 100)
+    private String name;
+
+    public Student() {
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
