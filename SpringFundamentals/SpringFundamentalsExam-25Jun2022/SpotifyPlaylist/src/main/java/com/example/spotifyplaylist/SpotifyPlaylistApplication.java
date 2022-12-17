0543ffbb25ed6cc8abaa05cedc90e1eec7354bc5
Sand82 <sandoki@abv.bg>
2022-12-17 20:10:49 +0200

    added new exercise

---
+++ SpringFundamentals/SpringFundamentalsExam-25Jun2022/SpotifyPlaylist/src/main/java/com/example/spotifyplaylist/SpotifyPlaylistApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotifyplaylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyPlaylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyPlaylistApplication.class, args);
+	}
+
+}
---
+++ SpringFundamentals/SpringFundamentalsExam-25Jun2022/SpotifyPlaylist/src/test/java/com/example/spotifyplaylist/SpotifyPlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotifyplaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyPlaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
